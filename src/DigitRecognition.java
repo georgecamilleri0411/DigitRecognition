c7f315551ccd97034d069ea7d3dd5e8141dcbd5a
George Camilleri <georgecamilleri@gmail.com>
2022-01-19 21:19:37 +0100

    20220119T211900 - Nearest Neighbour ready

--- src/DigitRecognition.java
+++ src/DigitRecognition.java
@@ -1,15 +1,24 @@
 public class DigitRecognition {
 
 	public static void main (String args[]) {
-		FileReader trainingData = new FileReader();
-		trainingData.readFile("files/cw2DataSet1.csv");
-
-		FileReader testingData = new FileReader();
-		testingData.readFile("files/cw2DataSet2.csv");
+		FileReader trainingData = loadFile ("cw2DataSet2.csv");
+		FileReader testData = loadFile("cw2DataSet1.csv");
 
 		// TEST
-		System.out.println (trainingData.imageList.get(0).getEuclideanDistance(testingData.imageList.get(0)));
+		NearestNeighbour nn = new NearestNeighbour (trainingData, testData);
+		int answer = -1;
+		for (int i = 0; i < testData.imageList.size(); i++) {
+			answer = nn.findNearestImage(testData.imageList.get(i));
+			//System.out.println("NearestNeighbour: " + testData.imageList.get(i).getDigitValue() + ": Index = " + answer + " (" + trainingData.imageList.get(answer).getDigitValue() + ")");
+			System.out.println(testData.imageList.get(i).getDigitValue() + ";" + trainingData.imageList.get(answer).getDigitValue());
+		}
 		// END TEST
+	}
+
+	public static FileReader loadFile (String fileName) {
+		FileReader f = new FileReader();
+		f.readFile("files/" + fileName);
+		return f;
 	}
 
 }
--- src/NearestNeighbour.java
+++ src/NearestNeighbour.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class NearestNeighbour {
+public class NearestNeighbour {
+
+	private FileReader trainingFile;
+	private FileReader testFile;
+
+	public NearestNeighbour (FileReader _trainingFile, FileReader _testFile) {
+		this.trainingFile = _trainingFile;
+		this.testFile = _testFile;
+	}
+
+	/*
+	Finds the nearest neighbour from the trainingFile to the testImage passed as argument
+	 */
+	public int findNearestImage (Image testImage) {
+		try {
+			int imageIndex = -1;
+			double nearest = Double.MAX_VALUE;
+			double current = 0;
+			for (int i = 0; i < this.trainingFile.imageList.size(); i++) {
+				current = (testImage.getEuclideanDistance(this.trainingFile.imageList.get(i)));
+				if (current < nearest) {
+					nearest = current;
+					imageIndex = i;
+				}
+			}
+			return imageIndex;
+		} catch (Exception e) {
+			System.out.println ("NearestNeighbour.findNearestImage - an error has been found: " + e.getMessage());
+			return -1;
+		}
+	}
+
 }
